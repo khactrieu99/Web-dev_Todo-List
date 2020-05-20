c938a16db1320ff668dfb5ee1cdb16bf1bdc56b4
Trieu <khactrieu.personal@gmail.com>
2020-05-21 00:44:23 +0700

    finish

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-var tasksID = [];
-var taskItem = []
 
 const TICKER_DIV = '<div class="ticker"><i class="far fa-circle"></i></div>';
 const DELETE_DIV = '<div class="delete"><i class="far fa-trash-alt"></i></div>';
+
+var tasks = localStorage.getItem('tasks')?JSON.parse(localStorage.getItem('tasks')):[];
+var taskItem = [];
+var tasksID = [];
 
 
 var render = function () {
@@ -14,8 +16,6 @@ var render = function () {
 
 var addTask = function(task) {
     var id = makeID();
-    tasksID.push(id);
-
     var li = document.createElement('li');
 
     li.innerHTML = TICKER_DIV + '<div class="content">' + task + '</div>' + DELETE_DIV;
@@ -25,24 +25,46 @@ var addTask = function(task) {
     document.getElementById('close').click();
 
     taskItem.push(li);
+    tasksID.push(id);
+    tasks.push({id:id, name:task});
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 
     li.getElementsByClassName('delete')[0].onclick = () => {
-        var index = tasksID.indexOf(id);
-        if(index > -1) {
-            tasksID.splice(index, 1);
-            taskItem.splice(index, 1);
+        // user must confirm to delete
+        var confirmation = confirm("Do you want to delete " + "'" + task + "' away from list?");
+
+        if(confirmation) {
+            deleteTask(id);
+            var index = tasksID.indexOf(id);
+            if(index > -1) {
+                tasksID.splice(index, 1);
+                taskItem.splice(index, 1);
+            }
+            else console.log('ko dc')
+            render();
         }
-        else console.log('ko dc')
-        render();
     }
 
     li.getElementsByClassName('ticker')[0].onclick = () => {
-        var i = document.createElement('i');
+        deleteTask(id);
         li.getElementsByClassName('ticker')[0].innerHTML = '<i class="fas fa-circle"></i>';
         li.getElementsByClassName('content')[0].style.textDecoration = "line-through";
-        li.getElementsByClassName('delete')[0].style.visibility = "hidden";
         render();
     }
+}
+
+var deleteTask = function(id) {
+    var index = -1;
+
+    tasks.forEach((task, pos) => {
+        if(task.id === id) index = pos;
+    });
+
+    if(index > -1)
+        tasks.splice(index, 1);
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
 }
 
 var checkTask = function() {
@@ -53,6 +75,12 @@ var checkTask = function() {
     addTask(task);
 
     document.getElementById('task').value = "";
+}
+
+var enterTask = function (event) {
+    if(event.keyCode == 13) {
+        document.getElementById('add-task').click();
+    }
 }
 
 var makeID = function () {
@@ -69,4 +97,25 @@ var makeID = function () {
     return result;
  }
 
+
+window.onload = function() {
+    document.getElementById('list-items').innerHTML = "";
+    render();
+
+    var tempTasks = [];
+
+    this.tasks.forEach(task => {
+        tempTasks.push(task);
+    });
+
+    this.tasks = [];
+
+    tempTasks.forEach(task => {
+        addTask(task.name);
+    });
+};
+
+
+
 document.getElementById('add-task').addEventListener('click', checkTask);
+document.getElementById('task').onkeypress = enterTask;
